63394546b1dd6d2387209e2089b47c1d8877366e
Robert Mozayeni <dev@mozayeni.com>
2014-09-13 13:24:06 -0400

    Personal info and angles

---
+++ Scolio/AngleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AngleViewController.swift
+//  Scolio
+//
+//  Created by Robert S Mozayeni on 9/13/14.
+//  Copyright (c) 2014 Robert Mozayeni. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class AngleViewController: UIViewController {
+    let motionManager = CMMotionManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue(), withHandler: { (accelData, error) -> Void in
+            let angle = atan2(accelData.acceleration.x, accelData.acceleration.y) * 180 / M_PI
+            println("DATA: \(angle)")
+        })
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ Scolio/PersonalInfoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PersonalInfoViewController.swift
+//  Scolio
+//
+//  Created by Robert S Mozayeni on 9/13/14.
+//  Copyright (c) 2014 Robert Mozayeni. All rights reserved.
+//
+
+import UIKit
+
+class PersonalInfoViewController: UIViewController {
+    @IBOutlet var ageTextField: UITextField!
+    @IBOutlet var biologicalSexSegmentedControl: UISegmentedControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ageTextField.becomeFirstResponder()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
